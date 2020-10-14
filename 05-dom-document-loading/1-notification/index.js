c03519de1d10fd705952da87705e1d863ff6a372
Andrey Sergeev <sergeev_aa@krista.ru>
2020-10-14 19:55:12 +0300

    4.1 task

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,53 @@
 export default class NotificationMessage {
 
+  element;
+  secDuration;
+
+  constructor(message = '', {duration = 0, type = ''} = {}) {
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+
+    if (NotificationMessage.showed) {
+      NotificationMessage.showed.remove();
+    }
+
+    this.render();
+  }
+
+  show(toDocument = document.body) {
+    toDocument.append(this.element);
+    setTimeout(() => this.remove(), this.duration);
+    return this.element;
+  }
+
+  render() {
+    const element = document.createElement('div');
+    element.innerHTML = `
+        <div class="notification ${this.type}" style="--value:${this.getSecDuration()}">
+            <div class="timer"></div>
+            <div class="inner-wrapper">
+            <div class="notification-header">${this.type}</div>
+            <div class="notification-body">
+                ${this.message}
+            </div>
+        </div>
+    </div>
+    `;
+    this.element = element.firstElementChild;
+    NotificationMessage.showed = this.element;
+  }
+
+  getSecDuration() {
+    return this.duration / 1000;
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+  }
+
 }
