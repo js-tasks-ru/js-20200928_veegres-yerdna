fc403a9f35192a43b1b5481f1aee5edc2f20e437
Andrey <andrewsergeev19@gmail.com>
2020-10-14 22:36:47 +0300

    4.2 task

--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -1,4 +1,98 @@
 export default class SortableTable {
 
+  element;
+
+  constructor(header = [], {data} = {}) {
+    this.header = header;
+    this.data = data;
+
+    this.render();
+  }
+
+  sort(fieldValue, orderValue) {
+    switch (fieldValue) {
+      case 'title': {
+        sortStrings();
+        break;
+      }
+      case 'quantity':
+      case 'price':
+      case 'sales':
+        break;
+      default:
+        return;
+    }
+    const sortedData = this.sortData(fieldValue, orderValue, this.data);
+
+  }
+
+  sortStrings(arr, param) {
+    const resultArr = [...arr];
+    const collator = new Intl.Collator('ru', {caseFirst: "upper"});
+    if (param === 'asc') {
+      return resultArr.sort((a, b) => collator.compare(a, b));
+    } else if (param === 'desc') {
+      return resultArr.sort((a, b) => collator.compare(b, a));
+    }
+    return resultArr;
+  }
+
+  render() {
+    const element = document.createElement('div');
+    element.innerHTML = this.createTable();
+    this.element = element.firstElementChild;
+  }
+
+  createHeaderRowColumn(id, title, order) {
+    return `
+        <div class="sortable-table__cell" data-id="${id}" data-sortable="false" data-order="${order}">
+            <span>${title}</span>
+            <span data-element="arrow" class="sortable-table__sort-arrow">
+                <span class="sort-arrow"></span>
+            </span>
+        </div>
+    `;
+  }
+
+  createHeaderRow() {
+    return `
+        <div data-element="header" class="sortable-table__header sortable-table__row">
+            ${this.header.map((value) => this.createHeaderRowColumn(value.id, value.title, value.order)).join('\n')}
+        </div>
+    `;
+  }
+
+  createDataRow(colInfo) {
+    return `
+        <a href="" class="sortable-table__row">
+            <div class="sortable-table__cell">
+                <img class="sortable-table-image" alt="Image" src="${colInfo.images[0].url}">
+            </div>
+            <div class="sortable-table__cell">${colInfo.title}</div>
+            <div class="sortable-table__cell">${colInfo.quantity}</div>
+            <div class="sortable-table__cell">${colInfo.price}</div>
+            <div class="sortable-table__cell">${colInfo.sales}</div>
+        </a>
+    `;
+  }
+
+  createTable() {
+    return `
+      <div class="sortable-table">
+        ${this.createHeaderRow()}
+        ${this.data.map((colInfo) => this.createDataRow(colInfo)).join('\n')}
+      </div>
+    `;
+
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+  }
+
 }
 
